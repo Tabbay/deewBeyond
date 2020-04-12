b7397496962e14f4e4315290163cbf72bc22f5c1
Garrett Tolbert <garrett1.tolbert@gmail.com>
2020-04-12 15:57:57 -0400

    fixed layout

--- src/components/InfoStrip/InfoStrip.js
+++ src/components/InfoStrip/InfoStrip.js
@@ -1,15 +1,14 @@
 import React, { useState } from 'react';
 import './infoStrip.css';
 
-const InfoStrip = () =>{
-
-  return (
-    <div className='infoStrip-wrapper'>
-      <div className='main'>
-        <div className="column" style={{ background: "lightblue" }} />
-        <div className="column" style={{ background: "#fcfcfc" }} />
-      </div>
-    </div>
-  );
-}
+const InfoStrip = () => {
+    return (
+        <div className='infoStrip-wrapper'>
+            <div className='home-main'>
+                <div className='column' style={{ background: 'lightblue' }} />
+                <div className='column' style={{ background: '#fcfcfc' }} />
+            </div>
+        </div>
+    );
+};
 export default InfoStrip;
